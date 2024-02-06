c73731781e01a978fabf573e27ddd137a7b64c4e
BratislavPanajotovic <panajotovicbratislav@gmail.com>
2024-02-06 12:54:57 +0100

    Game functionallity finished - next add table

--- functions.js
+++ functions.js
@@ -1,4 +1,4 @@
-let divTable = document.getElementById('table');
+let divTable = document.getElementById("table");
 let clickEventHandler;
 let selectedDifficulty;
 let currentUsername;
@@ -6,152 +6,157 @@ let timerStarted;
 let matchingCount = 0;
 
 function removeClickListener() {
-    divTable.removeEventListener('click', clickEventHandler);
+  divTable.removeEventListener("click", clickEventHandler);
 }
 
 function getRandomCards(src, count) {
-    let uniquePics = Array.from(new Set(src));
-    let selectedPics = uniquePics.slice(0, Math.ceil(count / 2));
-    let dblPics = selectedPics.flatMap(item => [item, item]);
-    return dblPics.slice(0, count);
+  let uniquePics = Array.from(new Set(src));
+  let selectedPics = uniquePics.slice(0, Math.ceil(count / 2));
+  let dblPics = selectedPics.flatMap((item) => [item, item]);
+  return dblPics.slice(0, count);
 }
 
 function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 }
 function getRowsColsFromDifficulty(difficulty) {
-    switch (difficulty) {
-        case 'beginner':
-            return 4;
-        case 'intermediate':
-            return 6;
-        case 'professional':
-            return 8;
-        case 'expert':
-            return 10;
-        default:
-            return 4; // Default to beginner if difficulty is not recognized
-    }
+  switch (difficulty) {
+    case "beginner":
+      return 4;
+    case "intermediate":
+      return 6;
+    case "professional":
+      return 8;
+    case "expert":
+      return 10;
+    default:
+      return 4; // Default to beginner if difficulty is not recognized
+  }
 }
 
-function picMaker(src, rows, cols, currentUsername, gameCompletionCallback) {
-    let shuffledSrc = shuffleArray([...src]);
-    let count = rows * cols;
-    let randomCards = getRandomCards(shuffledSrc, count);
-    shuffleArray(randomCards);
+function picMaker(src, rows, cols) {
+  let shuffledSrc = shuffleArray([...src]);
+  let count = rows * cols;
+  let randomCards = getRandomCards(shuffledSrc, count);
+  shuffleArray(randomCards);
 
-    let table = document.createElement('table');
-    let flippedCards = [];
-    let matchingCount = 0;
+  let table = document.createElement("table");
+  let flippedCards = [];
+  let matchingCount = 0;
 
-    function removeSrc() {
-        flippedCards.forEach(card => {
-            card.setAttribute('src', 'icons/questionmark.jpg');
-        });
+  function removeSrc() {
+    flippedCards.forEach((card) => {
+      card.setAttribute("src", "icons/questionmark.jpg");
+    });
+    flippedCards = [];
+  }
+
+  function handleClick() {
+    let currentRow = parseInt(this.getAttribute("data-row"));
+    let currentCol = parseInt(this.getAttribute("data-col"));
+    let img = this.querySelector("img");
+
+    if (!flippedCards.includes(img) && flippedCards.length < 2) {
+      img.setAttribute("src", randomCards[currentRow * cols + currentCol]);
+      flippedCards.push(img);
+
+      if (
+        flippedCards.length === 2 &&
+        flippedCards[0].src === flippedCards[1].src
+      ) {
+        matchingCount++;
         flippedCards = [];
-    }
 
-    function handleClick() {
-        let currentRow = parseInt(this.getAttribute('data-row'));
-        let currentCol = parseInt(this.getAttribute('data-col'));
-        let img = this.querySelector('img');
-
-        if (!flippedCards.includes(img) && flippedCards.length < 2) {
-            img.setAttribute('src', randomCards[currentRow * cols + currentCol]);
-            flippedCards.push(img);
-
-            if (flippedCards.length === 2 && flippedCards[0].src === flippedCards[1].src) {
-                matchingCount++;
-                flippedCards = [];
-
-                if (matchingCount === count / 2) {
-                    checkGameCompletion();
-                }
-            } else if (flippedCards.length === 2) {
-                setTimeout(removeSrc, 2000);
-            }
+        if (matchingCount === count / 2) {
+          checkGameCompletion();
         }
+      } else if (flippedCards.length === 2) {
+        setTimeout(removeSrc, 2000);
+      }
+    }
+  }
+
+  for (let i = 0; i < rows; i++) {
+    let row = document.createElement("tr");
+
+    for (let j = 0; j < cols; j++) {
+      let cell = document.createElement("td");
+      cell.setAttribute("class", "card");
+      cell.setAttribute("data-row", i);
+      cell.setAttribute("data-col", j);
+
+      let img = document.createElement("img");
+      img.setAttribute("class", "icon");
+      img.classList.add("default");
+      img.setAttribute("src", "icons/questionmark.jpg");
+
+      cell.addEventListener("click", handleClick);
+
+      cell.appendChild(img);
+      row.appendChild(cell);
     }
 
-    for (let i = 0; i < rows; i++) {
-        let row = document.createElement('tr');
+    table.appendChild(row);
+  }
 
-        for (let j = 0; j < cols; j++) {
-            let cell = document.createElement('td');
-            cell.setAttribute('class', 'card');
-            cell.setAttribute('data-row', i);
-            cell.setAttribute('data-col', j);
-
-            let img = document.createElement('img');
-            img.setAttribute('class', 'icon');
-            img.classList.add("default");
-            img.setAttribute('src', 'icons/questionmark.jpg');
-
-            cell.addEventListener('click', handleClick);
-
-            cell.appendChild(img);
-            row.appendChild(cell);
-        }
-
-        table.appendChild(row);
-    }
-
-    divTable.innerHTML = '';
-    divTable.appendChild(table);
-
-    function checkGameCompletion() {
-        const time = timerValue;
-        stopTimer();
-
-        let userResults = JSON.parse(localStorage.getItem(selectedDifficulty)) || [];
-
-        // Add the current result to the array
-        userResults.push({ username: currentUsername, time });
-
-        // Sort the results based on time in ascending order
-        userResults.sort((a, b) => a.time - b.time);
-
-        // Keep only the top 5 results
-        userResults = userResults.slice(0, 5);
-
-        // Save the updated results back to localStorage using the selectedDifficulty as the key
-        localStorage.setItem(selectedDifficulty || 'beginner', JSON.stringify(userResults));
-
-        gameCompletionCallback();  // Call the provided callback
-    }
+  divTable.innerHTML = "";
+  divTable.appendChild(table);
 }
 
 let timerValue = 0;
 let timerId;
 
 function updateTimer() {
-    timerValue++;
-    document.getElementById('timer').innerText = "Your time: ";
-    document.getElementById('timer').innerText += timerValue;
+  timerValue++;
+  document.getElementById("timer").innerText = "Your time: ";
+  document.getElementById("timer").innerText += timerValue;
 }
 
 function startTimer() {
-    console.log("Timer started");
-    timerId = setInterval(updateTimer, 1000);
+  console.log("Timer started");
+  timerId = setInterval(updateTimer, 1000);
 }
 
 function stopTimer() {
-    clearInterval(timerId);
+  clearInterval(timerId);
+  timerValue = 0;
+}
+
+function checkGameCompletion() {
+  const time = timerValue;
+  const playAgain = window.confirm(
+    `You won! Your time: ${time} seconds! Do you want to have a new try?`
+  );
+
+  if (playAgain) {
+    beginner.disabled = false;
+    intermediate.disabled = false;
+    professional.disabled = false;
+    expert.disabled = false;
+    timerStarted = false;
+    removeClickListener();
+    divTable.innerHTML = "";
+    stopTimer();
     timerValue = 0;
+    document.querySelector("#timer").innerHTML = "Your time: 0";
+  } else {
+    console.log(
+      "Game Over! All pairs matched, but the user chose not to play again."
+    );
+  }
 }
 
 export {
-    getRandomCards,
-    shuffleArray,
-    picMaker,
-    updateTimer,
-    startTimer,
-    stopTimer,
-    removeClickListener,
-    getRowsColsFromDifficulty
+  getRandomCards,
+  shuffleArray,
+  picMaker,
+  updateTimer,
+  startTimer,
+  stopTimer,
+  removeClickListener,
+  getRowsColsFromDifficulty,
 };
-
--- main.js
+++ main.js
@@ -1,20 +1,80 @@
-let src = ["icons/ai.png","icons/amazon.png","icons/apple.webp","icons/beats.png","icons/chrome.png","icons/clc.png","icons/css.png","icons/deezer.png","icons/docker.jpg","icons/e.png","icons/ea.png","icons/excell.png","icons/fb.png","icons/gh.webp","icons/ghome.webp","icons/git.png","icons/google.png","icons/gpt.png","icons/html.png","icons/http.png","icons/in.png","icons/insta-old.jpg","icons/it-bootcamp.jpg","icons/jordan.png","icons/js.png","icons/lens.png","icons/location.png","icons/mail.png","icons/microsoft.png","icons/msg.webp","icons/mssg.jpg","icons/netflix.png","icons/nike.png","icons/office.png","icons/php.png","icons/play.webp","icons/pp.png","icons/py.png","icons/React-icon.svg.png","icons/slack.png","icons/spotify.png","icons/teams.png","icons/telegram.png","icons/time.jpg","icons/time.jpg","icons/twitter.png","icons/viber.png","icons/wa.jpg","icons/worf.png","icons/yt.png","icons/gdrive.png"];
+import {
+  getRandomCards,
+  shuffleArray,
+  picMaker,
+  updateTimer,
+  startTimer,
+  stopTimer,
+  getRowsColsFromDifficulty,
+  removeClickListener,
+} from "./functions.js";
 
-import { getRandomCards, shuffleArray, picMaker, updateTimer, startTimer, stopTimer,getRowsColsFromDifficulty,removeClickListener } from './functions.js';
-
+let src = [
+  "icons/ai.png",
+  "icons/amazon.png",
+  "icons/apple.webp",
+  "icons/beats.png",
+  "icons/chrome.png",
+  "icons/clc.png",
+  "icons/css.png",
+  "icons/deezer.png",
+  "icons/docker.jpg",
+  "icons/e.png",
+  "icons/ea.png",
+  "icons/excell.png",
+  "icons/fb.png",
+  "icons/gh.webp",
+  "icons/ghome.webp",
+  "icons/git.png",
+  "icons/google.png",
+  "icons/gpt.png",
+  "icons/html.png",
+  "icons/http.png",
+  "icons/in.png",
+  "icons/insta-old.jpg",
+  "icons/it-bootcamp.jpg",
+  "icons/jordan.png",
+  "icons/js.png",
+  "icons/lens.png",
+  "icons/location.png",
+  "icons/mail.png",
+  "icons/microsoft.png",
+  "icons/msg.webp",
+  "icons/mssg.jpg",
+  "icons/netflix.png",
+  "icons/nike.png",
+  "icons/office.png",
+  "icons/php.png",
+  "icons/play.webp",
+  "icons/pp.png",
+  "icons/py.png",
+  "icons/React-icon.svg.png",
+  "icons/slack.png",
+  "icons/spotify.png",
+  "icons/teams.png",
+  "icons/telegram.png",
+  "icons/time.jpg",
+  "icons/time.jpg",
+  "icons/twitter.png",
+  "icons/viber.png",
+  "icons/wa.jpg",
+  "icons/worf.png",
+  "icons/yt.png",
+  "icons/gdrive.png",
+];
 
 let timerValue = 0;
 
-let beginner = document.querySelector('#beginner');
-let intermediate = document.querySelector('#intermediate');
-let professional = document.querySelector('#professional');
+let beginner = document.querySelector("#beginner");
+let intermediate = document.querySelector("#intermediate");
+let professional = document.querySelector("#professional");
 let expert = document.querySelector("#expert");
 
-let divTable = document.querySelector('#table');
-let username = document.querySelector('#user');
+let divTable = document.querySelector("#table");
+let username = document.querySelector("#user");
 
-let memoryGame = document.querySelector('#memoryGame');
-let showTimer = document.querySelector('#timer');
+let memoryGame = document.querySelector("#memoryGame");
+let showTimer = document.querySelector("#timer");
 
 showTimer.innerHTML = "";
 memoryGame.innerHTML = "";
@@ -23,99 +83,57 @@ intermediate.disabled = true;
 professional.disabled = true;
 expert.disabled = true;
 
-let currentUsername;
-
 username.addEventListener("keyup", function (event) {
-    if (event.keyCode === 13) {
-       let currentUsername = username.value.trim();
-        timerStarted = false;
-        if (currentUsername === '' || currentUsername.length <= 3) {
-            alert('Please enter a valid username!');
-        } else {
-            memoryGame.innerHTML = "Memory Game!";
-            beginner.disabled = false;
-            intermediate.disabled = false;
-            professional.disabled = false;
-            expert.disabled = false;
-            event.preventDefault();
-            username.placeholder = currentUsername;
-            stopTimer();
-        }
+  if (event.keyCode === 13) {
+    let currentUsername = username.value.trim();
+    if (currentUsername === "" || currentUsername.length <= 3) {
+      alert("Please enter a valid username!");
+    } else {
+      memoryGame.innerHTML = "Memory Game!";
+      beginner.disabled = false;
+      intermediate.disabled = false;
+      professional.disabled = false;
+      expert.disabled = false;
+      event.preventDefault();
+      username.placeholder = currentUsername;
+      stopTimer();
     }
+  }
 });
 
-
-
-let selectedDifficulty = 'beginner';
+let selectedDifficulty = "beginner";
 beginner.addEventListener("click", () => {
-    selectedDifficulty = 'beginner';
-    picMaker(src, 4, 4, currentUsername, handleGameCompletion);
+  selectedDifficulty = "beginner";
+  picMaker(src, 4, 4);
 });
 
 intermediate.addEventListener("click", () => {
-    selectedDifficulty = 'intermediate';
-    picMaker(src, 6, 6, currentUsername, handleGameCompletion);
+  selectedDifficulty = "intermediate";
+  picMaker(src, 6, 6);
 });
 
 professional.addEventListener("click", () => {
-    selectedDifficulty = 'professional';
-    picMaker(src, 8, 8, currentUsername, handleGameCompletion);
+  selectedDifficulty = "professional";
+  picMaker(src, 8, 8);
 });
 
 expert.addEventListener("click", () => {
-    selectedDifficulty = 'expert';
-    picMaker(src, 10, 10, currentUsername, handleGameCompletion);
+  selectedDifficulty = "expert";
+  picMaker(src, 10, 10);
 });
 
-function handleGameCompletion() {
-    stopTimer();
-    const time = timerValue;
-
-    let userResults = JSON.parse(localStorage.getItem(selectedDifficulty)) || [];
-
-    // Add the current result to the array
-    userResults.push({ username: currentUsername, time });
-
-    // Sort the results based on time in ascending order
-    userResults.sort((a, b) => a.time - b.time);
-
-    // Keep only the top 5 results
-    userResults = userResults.slice(0, 5);
-
-    // Save the updated results back to localStorage using the selectedDifficulty as the key
-    localStorage.setItem(selectedDifficulty || 'beginner', JSON.stringify(userResults));
-
-    const playAgain = window.confirm(`You won! Your time: ${time} seconds! Do you want to have a new try?`);
-
-    if (playAgain) {
-        beginner.disabled = false;
-        intermediate.disabled = false;
-        professional.disabled = false;
-        expert.disabled = false;
-        stopTimer();
-        timerValue = 0;
-        timerStarted = false;
-        removeClickListener();
-        divTable.innerHTML = "";
-
-        document.querySelector('#timer').innerHTML = "Your time: 0";
-    } else {
-        console.log("Game Over! All pairs matched, but the user chose not to play again.");
-    }
-}
 let timerStarted = false;
 
 const clickEventHandler = () => {
-    beginner.disabled = true;
-    intermediate.disabled = true;
-    professional.disabled = true;
-    expert.disabled = true;
+  beginner.disabled = true;
+  intermediate.disabled = true;
+  professional.disabled = true;
+  expert.disabled = true;
 
-    if (!timerStarted) {
-        startTimer();
-        timerStarted = true;
-    }
+  if (!timerStarted) {
+    startTimer();
+    timerStarted = true;
+  }
 };
-
 
 divTable.addEventListener("click", clickEventHandler);
